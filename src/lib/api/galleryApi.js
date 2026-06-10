2a533b3691424274a94ab6f8a6b901ffc89eee5a
올챙이 <126138033+sojeong0302@users.noreply.github.com>
2026-06-11 01:33:33 +0900

    refactor: api 요청 경로 수정

--- src/lib/api/galleryApi.js
+++ src/lib/api/galleryApi.js
@@ -1,8 +1,9 @@
 import { axiosInstance } from "@/lib/api/axiosInstance";
+import { API_ROUTES } from "../constants";
 
 export async function getMyGalleryCards({ page, limit, grade, genre, keyword }) {
   const response = await axiosInstance.get(
-    `/me/cards?page=${page}&limit=${limit}&grade=${grade}&genre=${genre}&keyword=${keyword}`,
+    `${API_ROUTES.GALLERY.MY_CARDS}?page=${page}&limit=${limit}&grade=${grade}&genre=${genre}&keyword=${keyword}`,
   );
 
   return response.data.data;
