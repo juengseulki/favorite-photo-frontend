1eb0ca1dc9a40ca82a7f5d4b59f46bef555b23cd
올챙이 <126138033+sojeong0302@users.noreply.github.com>
2026-06-10 21:20:52 +0900

    style: 반응형

--- src/app/my-gallery/page.js
+++ src/app/my-gallery/page.js
@@ -1,7 +1,6 @@
 "use client";
 
 import { PhotoCard } from "@/components/common/Card";
-import { axiosInstance } from "@/lib/api/axiosInstance";
 import { useAuth } from "@/providers/AuthProvider";
 import { useEffect, useState } from "react";
 import Button from "@/components/common/Button";
@@ -10,6 +9,7 @@ import Dropdown from "@/components/common/Dropdown";
 import Input from "@/components/common/Input";
 import Pagination from "@/components/common/Pagination";
 import { getMyGalleryCards } from "@/lib/api/galleryApi";
+import Image from "next/image";
 
 const GRADE_OPTIONS = [
   { label: "등급", value: "" },
@@ -80,26 +80,38 @@ export default function MyGalleryPage() {
   }, [meta.page, isLoading, grade, genre, meta.limit, keyword]);
 
   return (
-    <div className="max-w-[1920px] px-[220px]">
-      <div className="flex justify-between border-b-2 border-[#EEEEEE]">
+    <div className="mx-auto max-w-[1920px] px-[20px] desktop:px-[220px]">
+      <div className="hidden justify-between border-b-2 border-[#EEEEEE] desktop:flex">
         <span className="text-[62px] font-normal tracking-[-0.03em]">마이갤러리</span>
         <Button>포토카드 생성하기</Button>
       </div>
-      <div className="flex flex-col gap-[20px] mt-10 pb-10 border-b border-[#5A5A5A] ">
+      {/* 모바일 */}
+      <div className="fixed bottom-0 left-0 z-50 w-full px-[20px] pb-[20px] tablet:hidden">
+        <Button size="full">포토카드 생성하기</Button>
+      </div>
+      <div className="mt-10 flex flex-col gap-[20px] border-b border-[#5A5A5A] pb-3 desktop:pb-10">
         <p className="flex gap-[10px] items-center">
-          <span className="text-[24px] font-bold">
+          <span className="text-[14px] font-bold leading-none desktop:text-[24px]">
             {user ? `${user.nickname}님이 보유한 포토카드` : ""}
           </span>
-          <span className="text-[20px] text-[#A4A4A4] font-normal">{`(${meta.totalCopyCount}장)`}</span>
+          <span className="text-[12px] font-normal leading-none text-[#A4A4A4] desktop:text-[20px]">
+            ({meta.totalCopyCount}장)
+          </span>
         </p>
-        <div className="flex gap-[20px]">
-          {grades.map(({ grade, count }) => (
-            <GradeChip key={grade} grade={grade} count={count} />
-          ))}
+        <div className="overflow-x-auto">
+          <div className="flex w-max gap-[10px]">
+            {grades.map(({ grade, count }) => (
+              <GradeChip key={grade} grade={grade} count={count} />
+            ))}
+          </div>
         </div>
       </div>
-      <div className="flex gap-3 mt-3 mb-15">
+      <div className="mt-3 mb-3 flex justify-between gap-3 tablet:mb-20 tablet:justify-start">
+        <div className="flex h-[50px] w-[50px] shrink-0 items-center justify-center border border-[#DDDDDD] bg-[#0F0F0F] tablet:hidden">
+          <Image src="/img/icons/filter.png" alt="모바일 필터 버튼" width={15} height={15} />
+        </div>
         <Input
+          inputClassName="w-full"
           labelSize="md"
           variant="search"
           placeholder="검색"
@@ -107,44 +119,52 @@ export default function MyGalleryPage() {
           value={keyword}
           onChange={(e) => setKeyword(e.target.value)}
         />
-        <Dropdown
-          placeholder="등급"
-          size="sort"
-          options={GRADE_OPTIONS}
-          value={grade}
-          onChange={setGrade}
-        />
-        <Dropdown
-          placeholder="장르"
-          size="sort"
-          options={GENRE_OPTIONS}
-          value={genre}
-          onChange={setGenre}
-        />
+
+        <div className="hidden tablet:block">
+          <Dropdown
+            placeholder="등급"
+            size="sort"
+            options={GRADE_OPTIONS}
+            value={grade}
+            onChange={setGrade}
+          />
+        </div>
+
+        <div className="hidden tablet:block">
+          <Dropdown
+            placeholder="장르"
+            size="sort"
+            options={GENRE_OPTIONS}
+            value={genre}
+            onChange={setGenre}
+          />
+        </div>
       </div>
-      <div className="grid grid-cols-3 gap-30">
+      <div className="grid grid-cols-2 gap-[20px] desktop:grid-cols-3 desktop:gap-[120px]">
         {cards.map((card) => {
           return <PhotoCard key={card.id} card={card} />;
         })}
       </div>
-      <Pagination
-        page={meta.page}
-        totalCount={meta?.totalCount ?? 0}
-        pageSize={meta.limit}
-        onPageChange={(page) =>
-          setMeta((prev) => ({
-            ...prev,
-            page,
-          }))
-        }
-        onPageSizeChange={(pageSize) =>
-          setMeta((prev) => ({
-            ...prev,
-            pageSize,
-            page: 1,
-          }))
-        }
-      />
+      <div className="hidden tablet:block">
+        <Pagination
+          page={meta.page}
+          totalCount={meta?.totalCount ?? 0}
+          pageSize={meta.limit}
+          onPageChange={(page) =>
+            setMeta((prev) => ({
+              ...prev,
+              page,
+            }))
+          }
+          onPageSizeChange={(pageSize) =>
+            setMeta((prev) => ({
+              ...prev,
+              pageSize,
+              page: 1,
+            }))
+          }
+        />
+      </div>
     </div>
   );
 }
