56b56df77f865eb260cfedbcb15c9a92962a6d85
올챙이 <126138033+sojeong0302@users.noreply.github.com>
2026-06-11 14:48:22 +0900

    feat: 에러 처리

--- src/app/cards/create/page.jsx
+++ src/app/cards/create/page.jsx
@@ -9,6 +9,7 @@ import { GRADE_OPTIONS, GENRE_OPTIONS } from "@/lib/constants/galleryOptions";
 import { postPhotoCards } from "@/lib/api/galleryApi";
 import FileInput from "@/components/common/FileInput";
 import { useRouter } from "next/navigation";
+import { ERROR_MESSAGES } from "@/lib/constants";
 
 export default function CreatePhotoCard() {
   const router = useRouter();
@@ -20,6 +21,39 @@ export default function CreatePhotoCard() {
   const [genre, setGenre] = useState("");
   const [initialPrice, setInitialPrice] = useState("");
   const [totalQuantity, setTotalQuantity] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    const newErrors = {};
+
+    if (!name.trim()) {
+      newErrors.name = ERROR_MESSAGES.CARD_NAME_REQUIRED;
+    }
+
+    if (!imageFile) {
+      newErrors.imageFile = ERROR_MESSAGES.CARD_IMAGE_REQUIRED;
+    }
+
+    if (!grade) {
+      newErrors.grade = ERROR_MESSAGES.CARD_GRADE_REQUIRED;
+    }
+
+    if (!genre) {
+      newErrors.genre = ERROR_MESSAGES.CARD_GENRE_REQUIRED;
+    }
+
+    if (!initialPrice) {
+      newErrors.initialPrice = ERROR_MESSAGES.CARD_PRICE_REQUIRED;
+    }
+
+    if (!totalQuantity) {
+      newErrors.totalQuantity = ERROR_MESSAGES.CARD_QUANTITY_REQUIRED;
+    }
+
+    setErrors(newErrors);
+
+    return Object.keys(newErrors).length === 0;
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -33,7 +67,7 @@ export default function CreatePhotoCard() {
     formData.append("initialPrice", initialPrice);
     formData.append("totalQuantity", totalQuantity);
     formData.append("image", imageFile);
-
+    if (!validateForm()) return;
     try {
       await postPhotoCards(formData);
       router.push("/my-gallery");
@@ -51,44 +85,66 @@ export default function CreatePhotoCard() {
         onSubmit={handleSubmit}
         className="flex flex-col justify-center items-center gap-[65px] mb-30"
       >
-        <Input
-          label={"포토카드 이름"}
-          placeholder="포토카드 이름을 입력해주세요"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <Dropdown
-          label={"등급"}
-          placeholder="등급을 선택해 주세요"
-          options={GRADE_OPTIONS}
-          value={grade}
-          onChange={setGrade}
-        />
-        <Dropdown
-          label={"장르"}
-          placeholder="장르를 선택해 주세요"
-          options={GENRE_OPTIONS}
-          value={genre}
-          onChange={setGenre}
-        />
-        <Input
-          label={"가격"}
-          placeholder="가격을 입력해 주세요"
-          value={initialPrice}
-          onChange={(e) => setInitialPrice(e.target.value)}
-        />
-        <Input
-          label={"총 발행량"}
-          placeholder="총 발행향을 입력해주세요"
-          value={totalQuantity}
-          onChange={(e) => setTotalQuantity(e.target.value)}
-        />
-        <FileInput
-          label="사진 업로드"
-          onChange={(file) => {
-            setImageFile(file);
-          }}
-        />
+        <div className="flex flex-col gap-[10px]">
+          <Input
+            label={"포토카드 이름"}
+            placeholder="포토카드 이름을 입력해주세요"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          {errors.name && <p className="text-[16px] text-red">{errors.name}</p>}
+        </div>
+        <div className="flex flex-col gap-[10px]">
+          <Dropdown
+            label={"등급"}
+            placeholder="등급을 선택해 주세요"
+            options={GRADE_OPTIONS}
+            value={grade}
+            onChange={setGrade}
+          />
+          {errors.grade && <p className="text-[16px] text-red">{errors.grade}</p>}
+        </div>
+
+        <div className="flex flex-col gap-[10px]">
+          <Dropdown
+            label={"장르"}
+            placeholder="장르를 선택해 주세요"
+            options={GENRE_OPTIONS}
+            value={genre}
+            onChange={setGenre}
+          />
+          {errors.genre && <p className="text-[16px] text-red">{errors.genre}</p>}
+        </div>
+
+        <div className="flex flex-col gap-[10px]">
+          <Input
+            label={"가격"}
+            placeholder="가격을 입력해 주세요"
+            value={initialPrice}
+            onChange={(e) => setInitialPrice(e.target.value)}
+          />
+          {errors.initialPrice && <p className="text-[16px] text-red">{errors.initialPrice}</p>}
+        </div>
+
+        <div className="flex flex-col gap-[10px]">
+          <Input
+            label={"총 발행량"}
+            placeholder="총 발행향을 입력해주세요"
+            value={totalQuantity}
+            onChange={(e) => setTotalQuantity(e.target.value)}
+          />
+          {errors.totalQuantity && <p className="text-[16px] text-red">{errors.totalQuantity}</p>}
+        </div>
+
+        <div className="flex flex-col gap-[10px]">
+          <FileInput
+            label="사진 업로드"
+            onChange={(file) => {
+              setImageFile(file);
+            }}
+          />
+          {errors.imageFile && <p className="text-[16px] text-red">{errors.imageFile}</p>}
+        </div>
         <Textarea
           label={"포토카드 설명"}
           placeholder="카드 설명을 입력해 주세요"
