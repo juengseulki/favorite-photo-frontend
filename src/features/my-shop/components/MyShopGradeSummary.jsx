d1fc7e489d44538d0a8fd102596794f17dc69510
narin116 <aasdff1010@gmail.com>
2026-06-12 18:46:30 +0900

    feat: myShopSummary 작성 완료

--- src/features/my-shop/components/MyShopGradeSummary.jsx
+++ src/features/my-shop/components/MyShopGradeSummary.jsx
@@ -1,7 +1,39 @@
-"use client";
+import { GradeChip } from "@/components/common/Grade";
+import { useAuth } from "@/providers/AuthProvider";
 
-const MyShopGradeSummary = () => {
-  return <div>MyShopGradeSummary</div>;
+const MyShopGradeSummary = ({ meta, grades }) => {
+  let { user } = useAuth();
+
+  //TODO: 나중에 가능하다면 다른 파일로 분리하기
+  const SIZE = {
+    MOBILE: "sm",
+    TABLET: "md",
+    DESKTOP: "lg",
+  };
+
+  return (
+    <div>
+      <div>
+        <div>{user?.nickname}님이 보유하신 카드</div>
+        <div>{meta?.totalCopyCount}장</div>
+      </div>
+      <div className="tablet:hidden flex gap-[10px]">
+        {grades.map(({ grade, count }) => (
+          <GradeChip key={grade} grade={grade} count={count} size={SIZE.MOBILE} />
+        ))}
+      </div>
+      <div className="hidden tablet:flex desktop:hidden gap-[10px]">
+        {grades.map(({ grade, count }) => (
+          <GradeChip key={grade} grade={grade} count={count} size={SIZE.TABLET} />
+        ))}
+      </div>
+      <div className="hidden desktop:flex  gap-[20px]">
+        {grades.map(({ grade, count }) => (
+          <GradeChip key={grade} grade={grade} count={count} size={SIZE.DESKTOP} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyShopGradeSummary;
