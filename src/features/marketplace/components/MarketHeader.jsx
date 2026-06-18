14929f386deef96dbd16e719523dd8b879935a27
jyjy369 <jyjy369@gmail.com>
2026-06-18 17:45:02 +0900

    fix: 마켓플레이스 목록 반응형 레이아웃 수정

--- src/features/marketplace/components/MarketHeader.jsx
+++ src/features/marketplace/components/MarketHeader.jsx
@@ -2,18 +2,28 @@ import Button from "@/components/common/Button";
 
 export default function MarketHeader({ onSellClick }) {
   return (
-    <div className="flex items-center justify-between border-b-2 border-gray-100 pb-[20px] tablet:pb-[30px]">
-      <h1 className="font-brand text-[28px] font-bold leading-none text-white tablet:text-[42px] desktop:text-[62px]">
-        마켓플레이스
-      </h1>
+    <>
+      <div className="hidden items-center justify-between border-b-2 border-gray-100 pb-[20px] tablet:flex tablet:pb-[30px]">
+        <h1 className="font-brand text-[42px] font-bold leading-none text-white desktop:text-[62px]">
+          마켓플레이스
+        </h1>
+
+        <Button
+          type="button"
+          onClick={onSellClick}
+          className="hidden tablet:flex tablet:h-[50px] tablet:w-[300px] tablet:text-[14px] desktop:h-[60px] desktop:w-[440px] desktop:text-[18px]"
+        >
+          나의 포토카드 판매하기
+        </Button>
+      </div>
 
       <Button
         type="button"
         onClick={onSellClick}
-        className="hidden desktop:flex desktop:h-[60px] desktop:w-[440px] desktop:text-[18px]"
+        className="fixed bottom-[20px] left-[15px] right-[15px] z-20 flex h-[55px] rounded-[2px] text-[18px] tablet:hidden"
       >
         나의 포토카드 판매하기
       </Button>
-    </div>
+    </>
   );
 }
