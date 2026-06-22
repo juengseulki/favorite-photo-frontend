4b628c218b62bb76ab598e2463372ca5fbf09cba
올챙이 <126138033+sojeong0302@users.noreply.github.com>
2026-06-22 13:34:29 +0900

    style: 모바일 횟수 추가

--- src/app/cards/create/page.jsx
+++ src/app/cards/create/page.jsx
@@ -47,18 +47,20 @@ export default function CreatePhotoCard() {
   return (
     <ProtectedRoute>
       <div className="mx-auto max-w-[1920px] px-[20px] tablet:px-[60px] desktop:px-[220px] gap-[80px]">
-        <div className="hidden tablet:flex justify-between border-b-2 border-gray-100 mb-10 items-center">
-          <span className="font-brand text-[40px] desktop:text-[50px] font-normal tracking-[-0.03em]">
+        <div className="flex justify-between mb-5 items-center tablet:border-b-2 tablet:border-gray-100 tablet:mb-10">
+          <span className="hidden tablet:block font-brand text-[40px] desktop:text-[50px] font-normal tracking-[-0.03em]">
             포토카드 생성
           </span>
+
           <div className="flex items-baseline gap-[12px]">
             <div className="flex items-baseline gap-[4px]">
-              <span className="text-main text-[40px]">{remainingCreateCount}</span>
-
-              <span className="text-[28px] l">/{monthlyCreateLimit}</span>
+              <span className="text-main text-[32px] tablet:text-[40px]">
+                {remainingCreateCount}
+              </span>
+              <span className="text-[20px] tablet:text-[28px]">/{monthlyCreateLimit}</span>
             </div>
 
-            <span className="text-gray-300 text-[16px] ">
+            <span className="text-gray-300 text-[16px]">
               ({year}년 {month}월)
             </span>
           </div>
